a1abe861b2284889479398ca4b880b37ae502061
Caroline Lücking <caroline.luecking@tu-braunschweig.de>
2025-01-07 22:11:36 +0100

    headway und swapping1 läuft mit idealen objective

--- src/access_file.py
+++ src/access_file.py
@@ -90,7 +90,8 @@ def big_H(a, b):
 def save_result(solver, vars, max_operatins: list, trainss, resources: list):
     events = []
     opdelay = 0
-    resource_graphes = timeslot_resource_graphes(solver, vars, trainss, resources)
+    resource_graphes = timeslot_resource_graphes(
+        solver, vars, trainss, resources)
     used_timeslots = []
     for time_index, timeslot in enumerate(vars):
         for train_index, train in enumerate(timeslot):
@@ -98,15 +99,22 @@ def save_result(solver, vars, max_operatins: list, trainss, resources: list):
                 if solver.Value(operation):
 
                     op = trainss[train_index][operation_index]
+                    if time_index == 0:
+                        opdelay += (op.coeff*max(0, time_index-op.threshold) +
+                                    op.increment*big_H(time_index, op.threshold))
+                        event = {"time": time_index, "train": train_index,
+                                 "operation": operation_index}
+                        events.append(event)
 
-                    if time_index == 0 or not solver.Value(vars[time_index-1][train_index][operation_index]):
+                    elif not solver.Value(vars[time_index-1][train_index][operation_index]):
                         if max_operatins[train_index] >= operation_index:
-                            opdelay += (op.coeff*max(0, time_index-op.threshold) +
-                                        op.increment*big_H(time_index, op.threshold))
-                            event = {"time": time_index, "train": train_index,
+                            opdelay += (op.coeff*max(0, time_index-1-op.threshold) +
+                                        op.increment*big_H(time_index-1, op.threshold))
+                            event = {"time": time_index-1, "train": train_index,
                                      "operation": operation_index}
                             if time_index != 0 and time_index not in used_timeslots:
                                 used_timeslots.append(time_index)
+
                             events.append(event)
                         if max_operatins[train_index] == operation_index:
                             max_operatins[train_index] = 0
@@ -183,6 +191,5 @@ def sort_events(events, time_events, graph):
     for event in time_events:
         if event not in sorted_time_events:
             sorted_time_events.append(event)
-
 
     return before + sorted_time_events + after
